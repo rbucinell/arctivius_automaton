d959db78a3abc0b4c4c04d501269195ded2dd2ed
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-06-28 01:15:49 -0400

    adding specializations to api wrapper

---
+++ src/gw2api/v2/specializations/specializations.js
@@ -0,0 +1,33 @@
+import { get } from '../../util/request.js';
+import { Specialization } from '../models/specialization.js';
+import { readFile } from 'fs/promises';
+
+export default class specializations
+{    
+    /**
+     * If accessed without specifying an id, a list of all ids is returned.
+     * 
+     * @param {string} specializationId (optional) The specialization id to look up.
+     * @param {boolean} cached (optional) Use a cached local copy of results from 2023-06-27. Default true.
+     * @returns {Array<Specialization>} A list of gw2 specializations
+     */
+    static async get( specializationId = '', cached = true )
+    {
+        specializationId = specializationId === '' ? `all`: specializationId;
+        if( cached ) return await specializations.cached( specializationId );
+        let response = await get(`specializations?ids=${specializationId}`);
+        return response.data.map( s => Specialization.parse( s ));
+    }
+
+    static async cached( specialiazationId = '' )
+    {
+        let cache = JSON.parse( await readFile('./src/gw2api/v2/specializations/cache.json'));
+        cache = cache.map( s => Specialization.parse(s));
+        if( specialiazationId !== '' && specialiazationId !== 'all' )
+        {
+            cache = cache.filter( s => s.id === specialiazationId );
+        }
+        return cache;
+    }
+
+}
