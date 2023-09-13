de67fbed1508877615104d32c18411d0b1e9a5c9
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-09-13 02:14:02 -0400

    added error handling for logs

--- src/resources/gw2api/v2/guild/guild.js
+++ src/resources/gw2api/v2/guild/guild.js
@@ -1,6 +1,7 @@
 import { get } from '../../util/request.js';
-import {Guild} from '../models/guild.js'
-import {GuildEvent} from '../models/guildevent.js'
+import { Guild } from '../models/guild.js'
+import { GuildEvent } from '../models/guildevent.js'
+import { error } from '../../../../logger.js';
 
 export default class guild {
     
@@ -16,11 +17,22 @@ export default class guild {
 
     /**
      * 
-     * @param {string} guildId 
-     * @param {string} since (optional) 
+     * @param {string} guildId ID of the guild to query. Ensure API Key has guild owner perms.
+     * @param {string} since (optional) to filter the response to events with the ID after the provided ID
      * @returns {Array<GuildEvent>}
      */
     static async log( guildId, since=undefined ) {
-        return (await get(`guild/${guildId}/log${ since ? `?since=${since}` : ''}`)).data.map( e => GuildEvent.parse(e) );
+        let guildEvents = [];
+        try{
+            const response = await get(`guild/${guildId}/log${ since ? `?since=${since}` : ''}`)
+            if( response.data)
+            {
+                guildEvents = response.data.map( e => GuildEvent.parse(e) );
+            }
+        }catch( err )
+        {
+            error(err);
+        }
+        return guildEvents;        
     }
 }
