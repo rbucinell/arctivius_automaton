580fda4eda93955da8f516ca65ddc9b298b70b1b
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-03-04 15:34:51 -0500

    added functionality to apikey command

--- src/commands/apikey.js
+++ src/commands/apikey.js
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { info, error} from '../logger.js';
-import { AttendanceManager } from '../wvw/attendance/manager.js';
+import { getAPIKey,setAPIKey } from '../guild/guildlookup.js';
 
 export default class apikey {
 
@@ -23,23 +23,34 @@ export default class apikey {
                 .setDescription('remove your api key from records') );
     }
 
-    // interaction.guild is the object representing the Guild in which the command was run
     static async execute( interaction ) {
-        await interaction.deferReply();
-        const subCommand = interaction.options.getSubcommand();
-        if (!subCommand) {
-            return await interaction.reply('You need to provide a subcommand!');
-        }
+        try{
+            await interaction.deferReply();
 
-        // Here you can handle different subcommands
-        if (subCommand === 'set') {
-            await interaction.followUp({ content: 'You called set command. It does nothing yet, sorry', ephemeral: true });
-        } else if (subCommand === 'view') {
-            await interaction.followUp({ content: 'You called view command. It does nothing yet, sorry', ephemeral: true });
-        } else if( subCommand === 'clear' ) {
-            await interaction.followUp({ content: 'You called clear command. It does nothing yet, sorry', ephemeral: true });
-        } else {
-            await interaction.followUp('Invalid Command');
+            const subCommand = interaction.options.getSubcommand();
+            if (!subCommand) {
+                return await interaction.reply({content:'You need to provide a subcommand!',ephemeral:true});
+            }
+            let username = interaction.user.username;
+            info( `Discord User: ${interaction.user.id} (${username}) called apikey command: ${subCommand}`, true);
+            // Here you can handle different subcommands
+            if (subCommand === 'set') {
+                let apikey = interaction.options.data[0].options[0].value;
+                let success = await setAPIKey(username, apikey);
+                info( `Discord User: ${interaction.user.id} (${username}) setting apikey to ${apikey}. Success = ${success}`, true);
+                await interaction.followUp({ content: `${success? `Successfully set apikey to '\`${apikey}\`'.` : "Failed to set apikey"}`, ephemeral: true });
+            } else if (subCommand === 'view') {
+                let apikey = await getAPIKey( username );
+                await interaction.followUp({ content: `Automaton API Key found '\`${apikey}\`'`, ephemeral: true });
+            } else if( subCommand === 'clear' ) {
+                let success = await setAPIKey(username, '');
+                await interaction.followUp({ content: `${success? "Successfully cleared apikey." : "Failed to cleared apikey"}`, ephemeral: true });
+            } else {
+                await interaction.followUp({content:'Invalid Command',ephemeral:true});
+            }
+        }catch( err ){
+            error( err, true)
+            interaction.followUp( {content:`Error in command: ${err}`,ephemeral:true} );
         }
 
     }
