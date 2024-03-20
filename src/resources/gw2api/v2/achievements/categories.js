64e96ea684496a3c75b0db30624506fb66b084fd
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-03-20 16:36:11 -0400

    updated categories id parsing

--- src/resources/gw2api/v2/achievements/categories.js
+++ src/resources/gw2api/v2/achievements/categories.js
@@ -1,16 +1,28 @@
 import { get } from '../../util/request.js';
 import { AchievementCategory } from '../models/achievementscategory.js';
 
+const BASE = '/achievements/categories';
+
 export default class categories {
 
     static get WEEKLY_WVW_ID(){ return 346; }
 
     static async list(){
-        return (await get('/achievements/categories')).data;
+        return (await get(BASE)).data;
     }
 
-    static async get( id ){
-        return AchievementCategory.parse((await get(`/achievements/categories/${id}`)).data);
+    static async get( ...ids ){
+        if (ids.length === 0) {
+            return [];
+        }
+        else if( ids.length === 1 ){
+            const id = ids[0];
+            return AchievementCategory.parse((await get(`${BASE}/${id}`)).data);
+        }
+        else{
+            return (await get(`${BASE}?ids=${ids.join(',')}`)).data
+                .map( ac => AchievementCategory.parse(ac) );
+        }
     }
 }
 
