a28c32365788360504519ea782600231446f2428
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-05-10 00:59:32 -0400

    log update

--- tests/wvw/attendence/combatlog.test.js
+++ tests/wvw/attendence/combatlog.test.js
@@ -1,11 +1,11 @@
-import {describe, expect, test} from '@jest/globals';
-import * as CombatLogAttendance from '../../../src/wvw/attendance/combatlogattendance.js';
-jest.mock('ps-list', () => ({ __dirname: `C:\\Program Files\\nodejs` }))
+// import {describe, expect, test} from '@jest/globals';
+// import * as CombatLogAttendance from '../../../src/wvw/attendance/combatlogattendance.js';
+// jest.mock('ps-list', () => ({ __dirname: `C:\\Program Files\\nodejs` }))
 
-describe ('wvw attendance combat log module', () => {
+describe ('combatlog stub', () => {
 
     test('CombatLog Stub', () => {
-        expect(true);
+        expect(true).toBeTruthy();
     });
     
 });
