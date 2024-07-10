2e5799dcede5d6a9915c6a2e7cf6b2595a3f9567
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-07-10 02:03:37 -0400

    adding checkmark role

--- src/commands/register.js
+++ src/commands/register.js
@@ -3,8 +3,11 @@ import { info, warn, error, format } from '../logger.js';
 import { getGuildMemberByGW2Id, registerDiscordUserName } from "../guild/guildlookup.js";
 import { db, registrations, guilds, members } from '../resources/mongodb.js';
 import { DiscordManager } from "../discord/manager.js";
+import { CrimsonBlackout } from "../discord/ids.js";
 
 export default class register {
+
+    static get RoleId() { return CrimsonBlackout.ROLE_REGISTERED.description; }
 
     static get Name(){ return 'register'; }
     
@@ -41,12 +44,24 @@ export default class register {
                     const insertResponse = await registrations.insertOne( { discordId, gw2Id , date });
                     success = insertResponse.acknowledged;
                 }
+                if( success ){
+                    try{
+                        const guild = DiscordManager.Client.guilds.cache.get(CrimsonBlackout.GUILD_ID.description);
+                        let guildMembers = await guild.members.fetch();
+                        let user = guildMembers.find( _ => _.user.username === discordId );
+                        await user.roles.add( register.RoleId);
+                    }catch(err){
+                        error(err);
+                    }
+                }
                 await interaction.followUp({
                     content: `${success? "Successfully registered" : "Failed to register"} GuildWars2 ID: \`${ gw2Id }\``,
                     ephemeral: true
                 });
             }
             
+            
+
             let success = await registerDiscordUserName( gw2Id, discordId);
             info(`${format.command(this.Name, discordId)} PACK Doc: ${success? format.success("Successfully") : format.error("Failed to") } set discord username.`, true, true);
         }
