f3827b9d2f029be195984ae691439f5a9d3acf9a
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-08-27 09:35:47 -0400

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-
-
 import { DiscordManager } from './discord/manager.js';
 import { GuildVault } from './guild/guildvault.js';
 import { AttendanceManager } from './wvw/attendance/manager.js';
 import { MessageWatcher } from './discord/messagewatcher.js';
-import { ProgressManager } from './wvw/progress/progressmanager.js';
 import { SignupReminder } from './wvw/signup/sign-up-reminder.js';
 import { VoiceAttendence } from './wvw/attendance/voiceattendence.js';
 import { GuildSync } from './guild/guildsync.js';
+import { ProgressManager } from './wvw/progress/progressmanager.js';
 
 process.title = "[[ Arctivius Automaton ]]";
-
 await DiscordManager.Login();
 
+//TODO: Convert: MessageWatcher, AttendanceManager, SignupReminder, VoiceAttendence, ProgressManager, to extend Moudle
 //Initialize Moudles
-const modules = [ MessageWatcher, AttendanceManager, SignupReminder, VoiceAttendence, GuildVault, GuildSync ];
+const modules = [ MessageWatcher, AttendanceManager, SignupReminder, VoiceAttendence, GuildVault, GuildSync, /*ProgressManager*/ ];
 modules.forEach( m => m.initialize() );
-//ProgressManager.initialize();
-
-//TODO: Convert: MessageWatcher, AttendanceManager, SignupReminder, VoiceAttendence, to extend Moudle
